aaec72c85eb08e2cd94a570deb80820f6bba27da
quinnvaughn <qvaughn3@gmail.com>
2018-02-20 01:34:42 -0600

    Subscribe to mouse events.

---
+++ observable-subscribe-to-mouse-events.js
@@ -0,0 +1,23 @@
+import {Observable} from 'rxjs';
+
+let source = Observable.fromEvent(document, 'mousemove');
+
+source.subscribe(next, error, complete);
+
+function next(value: any) {
+  console.log('next: ', value);
+}
+
+function error(err: any) {
+  console.log('error: ', err);
+}
+
+function complete() {
+  console.log('complete');
+}
+
+Observable.fromEvent(document, 'mousemove')
+  .take(10)
+  .forEach(function next(value) {
+    console.log('next: ', value);
+  });
